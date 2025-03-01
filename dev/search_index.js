6b0e2cc7af2966946bbffa0c363d20b1b1e0e412
Documenter.jl <documenter@juliadocs.github.io>
2025-03-01 04:42:40 +0000

    build based on 1cfeb9a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OpticalFibers.jl","page":"OpticalFibers.jl","title":"OpticalFibers.jl","text":"","category":"section"},{"location":"","page":"OpticalFibers.jl","title":"OpticalFibers.jl","text":"Documentation for OpticalFibers.jl","category":"page"}]
+}
